d233d248627742303d0fa8f880140c56a51469b0
jyunoh18 <30875152+jyunoh18@users.noreply.github.com>
2017-08-28 08:45:19 -1000

    Create object.js

---
+++ object.js
@@ -0,0 +1,13 @@
+var x = prompt("first name?")
+
+var cole = {
+
+firstName: "Cole",
+lastName: "House",
+age: 15,
+grade: 10
+
+}
+
+window.alert(cole.firstName = x);
+window.alert(cole.firstName);
