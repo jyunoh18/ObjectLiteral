9e49ea233767673ba6f1f555ac7650fbffed2ff6
jyunoh18 <30875152+jyunoh18@users.noreply.github.com>
2017-08-30 08:24:55 -1000

    Update complexobject.js

--- complexobject.js
+++ complexobject.js
@@ -3,25 +3,25 @@ var Jyunghyun = {
 Name: "jyunghyun",
 age: 18,
 grade: 12,
-friends:["Justin","Michael","John"];
+friends:["Justin","Michael","John"]
 
-}
+};
 var Jyunghyun2 = {
 
 Name: "jyunghyunn",
 age: 17,
 grade: 11,
-friends:["Justinn","Michaell","Johnn"];
+friends:["Justinn","Michaell","Johnn"]
 
-}
+};
 var Jyunghyun3 = {
 
 Name: "jyunghyunnn",
 age: 16,
 grade: 10,
-friends:["Justinnn","Michaelll","Johnnn"];
+friends:["Justinnn","Michaelll","Johnnn"]
 
-}
+};
 
 var x = [Jyunghyun, Jyunghyun2, Jyunghyun3];
 window.alert(x[1].friends[0]);
