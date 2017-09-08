55bdf3e476382b0647f51005da0ab85062ab21b5
jyunoh18 <30875152+jyunoh18@users.noreply.github.com>
2017-09-08 08:36:35 -1000

    Create name.js
    The different responses will pop up on the screen every time the user answers the question.

---
+++ name.js
@@ -0,0 +1,19 @@
+var x = prompt("Name");
+document.getElementById('one').innerHTML = x;
+
+
+
+------------------------------------
+HTML part
+
+
+
+<p id="one"></p>
+
+-------------------------------------
+CSS Part 
+
+
+#one {
+  font-size: 200px;
+}
