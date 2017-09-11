0d21cd729a3c3a0fb170a42242fa209cc7521bc3
jyunoh18 <30875152+jyunoh18@users.noreply.github.com>
2017-09-11 08:11:17 -1000

    Create Eliminate.js

---
+++ Eliminate.js
@@ -0,0 +1,12 @@
+<p class="stem">Donald Trump</p>
+<p class="stem">Hitler</p>
+<p class="stem">Kim Jong Un</p>
+<p class="stem">Vladimir Putin</p>
+
+<button onclick="deathNote()">Random death</button>
+-------------------------------------------------
+
+function deathNote() {
+	var death = Math.floor(Math.random()*4);
+  document.getElementsByClassName('stem')[death].innerHTML = "xxxxxxxxxx"
+}
