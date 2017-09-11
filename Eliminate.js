b20962dc1a1929f953373d66e8ca74c5a6128b2e
jyunoh18 <30875152+jyunoh18@users.noreply.github.com>
2017-09-11 08:17:39 -1000

    Update Eliminate.js

--- Eliminate.js
+++ Eliminate.js
@@ -9,4 +9,12 @@
 function deathNote() {
 	var death = Math.floor(Math.random()*4);
   document.getElementsByClassName('stem')[death].innerHTML = "xxxxxxxxxx"
+	
+}
+
+--------------------------------------------------------
+	function deathNote() {
+	var death = Math.floor(Math.random()*4);
+  document.getElementsByClassName('stem')[death].innerHTML = 
+	  "<img src='https://www.singularityweblog.com/wp-content/uploads/2016/02/Death.jpg' width='10%'>";
 }
